5e701c712cc88a97ee9bb7e00ddb319e6f68a4b5
Cương Nguyên <nguyenduccuong1682001@gmail.com>
2022-07-07 13:36:29 +0700

    some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { Provider } from "react-redux";
 import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(
